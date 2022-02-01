bf49208cd05cf23d99d00227650fa4c6f0229671
Mahdy Noureddine <mahdy.a.noureddine@gmail.com>
2022-02-01 17:01:36 -0500

    commit tree 37798cebfda08ff37df4756bac4b961ae42309bb

--- app/controllers/plants_controller.rb
+++ app/controllers/plants_controller.rb
@@ -1,5 +1,5 @@
 class PlantsController < ApplicationController
-
+  wrap_parameters format: []
   # GET /plants
   def index
     plants = Plant.all
@@ -16,6 +16,20 @@ class PlantsController < ApplicationController
   def create
     plant = Plant.create(plant_params)
     render json: plant, status: :created
+  end
+
+  #PATCH /plants/:id
+  def update
+    plant = Plant.find_by(id: params[:id])
+    plant.update(plant_params)
+    render json: plant, status: :accepted
+  end
+
+  #DELETE /plants/:id
+  def destroy
+    plant = Plant.find_by(id: params[:id])
+    plant.destroy
+    head :no_content
   end
 
   private
